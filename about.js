b4908cd034e69ca14939825f8667b3ae25af9337
halwilcox <halwilcox09@gmail.com>
2021-09-03 13:10:55 -0600

    done

--- about.js
+++ about.js
@@ -1,14 +1,23 @@
 console.log("hello world");
 
+const img = document.querySelector('#img');
+img.addEventListener('mouseover', () => {
+	alert('Hello gorgeous!')
+})
 
 
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+const form = document.querySelector('form')
+form.addEventListener('submit', () => {
+	alert('Thank you for your response!');
+	})
+
---
+++ profile.js
@@ -0,0 +1,12 @@
+var color = document.getElementById("color");
+color.addEventListener('click', () => {
+    alert('Purple');
+})
+var place = document.getElementById("place")
+place.addEventListener('click', () => {
+    alert('Home');
+})
+var ritual = document.getElementById("ritual")
+ritual.addEventListener('click', () => {
+    alert('Sleep');
+})
